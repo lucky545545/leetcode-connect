6af88fe454ec55e2a63b4c348afa5b5825f1ffa2
lucky545545 <143895062+lucky545545@users.noreply.github.com>
2024-08-15 19:13:21 +0000

    Sync LeetCode submission - Lemonade Change (cpp)

---
+++ solutions/problems/lemonade_change/solution.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int count5 = 0;
+        int count10 = 0;
+        for(int i = 0 ; i<=bills.size() - 1 ; i++){
+            if(bills[i] == 5){
+                count5++;
+            }
+            else if(bills[i] == 10){
+                if(count5 >=1){
+                    count5--;
+                    count10++;
+                }
+                else{
+                    return false;
+                }
+            }
+            else if(bills[i] == 20){
+                if(count10 >= 1){
+                    count10--;
+                    if( count5 >= 1){
+                        count5--;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else if(count10 < 1){
+                    if(count5 >= 3){
+                        count5 = count5 -3;
+                    }
+                    else{
+                        return false;
+                    } 
+                }
+            }
+        }
+        return true;
+        
+    }
+};
