c7932a43b7ceaa55f0fa185f6b8eb089e69dff44
lucky545545 <143895062+lucky545545@users.noreply.github.com>
2025-12-19 16:58:04 +0000

    Sync LeetCode submission - Set Matrix Zeroes (cpp)

--- solutions/problems/set_matrix_zeroes/solution.cpp
+++ solutions/problems/set_matrix_zeroes/solution.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> setZeroes(vector<vector<int>>& matrix) {
-        unordered_set<int> row_no; 
-        unordered_set<int> column_no;
-        int n = matrix.size(); // number of rows
-        int m = matrix[0].size(); // number of columns
-        for(int i=0 ; i<n ; i++ ){
-            for(int j=0 ; j<m ; j++){
+        unordered_set<int> row_set;
+        unordered_set<int> col_set;
+        for(int i = 0 ; i < matrix.size() ; i++){
+            for(int j = 0 ; j < matrix[0].size() ; j++){
                 if(matrix[i][j] == 0){
-                    row_no.insert(i);
-                    column_no.insert(j);
-                    }
-                   
+                    row_set.insert(i);
+                    col_set.insert(j);
                 }
             }
-        for(int i=0 ; i<n ; i++ ){
-            for(int j=0 ; j<m ; j++){
-                if(row_no.count(i) > 0 || column_no.count(j) > 0){
+        }
+        for(int i = 0 ; i < matrix.size() ; i++){
+            for(int j = 0 ; j < matrix[0].size() ; j++){
+                if(row_set.count(i) > 0 || col_set.count(j) > 0){
                     matrix[i][j] = 0;
-                    }
-                   
                 }
             }
+        }
         return matrix;
-        
     }
 };
