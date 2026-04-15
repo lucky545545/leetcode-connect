03a7975ee2afb1ba0314fa68166bfce41061783c
lucky545545 <143895062+lucky545545@users.noreply.github.com>
2026-04-15 15:14:38 +0000

    Sync LeetCode submission - Fruit Into Baskets (cpp)

--- solutions/problems/fruit_into_baskets/solution.cpp
+++ solutions/problems/fruit_into_baskets/solution.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int, int> mp;
-        int j = 0; // Left pointer of our window
-        int out = 0; // Stores the max fruits found
-        
+        unordered_map<int , int> mp;
+        int ans = 0;
+        int count = 0;
+        int j = 0;
         for(int i = 0 ; i < fruits.size() ; i++){
-            // 1. Always add the current fruit to our baskets
             mp[fruits[i]]++;
-            
-            // 2. If we have MORE than 2 types of fruit, we must shrink the window
-            while(mp.size() > 2) {
-                // Remove the fruit at the left pointer
+            while(mp.size() > 2){
                 mp[fruits[j]]--;
-                
-                // If we dropped all fruits of this type, completely remove it from the map
-                if(mp[fruits[j]] == 0) {
+                if(mp[fruits[j]] == 0){
                     mp.erase(fruits[j]);
                 }
-                
-                // Shrink window
                 j++;
             }
-            
-            // 3. Update the max length (current index - left index + 1)
-            out = max(out, i - j + 1);
+            ans = max(ans , i-j+1);
         }
+        return ans;
         
-        return out;
     }
 };
