93f3d6d6679a90ac795af3f7270864dce84977d6
lucky545545 <143895062+lucky545545@users.noreply.github.com>
2026-06-08 10:20:31 +0000

    Sync LeetCode submission - Partition Array According to Given Pivot (cpp)

---
+++ solutions/problems/partition_array_according_to_given_pivot/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int> out;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[i] < pivot){
+                out.push_back(nums[i]);
+            }
+        }
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[i] == pivot){
+                out.push_back(nums[i]);
+            }
+        }
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[i] > pivot){
+                out.push_back(nums[i]);
+            }
+        }
+        return out;
+
+        
+    }
+};
