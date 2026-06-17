2c5b9a06b3a4db1a6f687933573c93f11f851a6e
lucky545545 <143895062+lucky545545@users.noreply.github.com>
2026-06-17 10:13:29 +0000

    Sync LeetCode submission - Daily Temperatures (cpp)

--- solutions/problems/daily_temperatures/solution.cpp
+++ solutions/problems/daily_temperatures/solution.cpp
@@ -5,12 +5,11 @@ public:
         vector<int> ans(temper.size() , 0);
         for(int i = 0 ; i < temper.size() ; i++){
             while(!st.empty() && temper[st.top()] < temper[i]){
-                ans[st.top()] = i - st.top();
-                st.pop();
+                ans[st.top()] = i - st.top() ;
+                st.pop(); 
             }
             st.push(i);
         }
         return ans;
-        
     }
 };
