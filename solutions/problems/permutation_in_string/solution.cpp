82c632d336accd17c5c81831858ea5ca5b7d0eb0
lucky545545 <143895062+lucky545545@users.noreply.github.com>
2026-07-26 20:15:18 +0000

    Sync LeetCode submission - Permutation in String (cpp)

---
+++ solutions/problems/permutation_in_string/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s1.size() > s2.size()){
+            return false;
+        }
+
+        vector<int> s1_count(26,0);
+        vector<int> s2_count(26,0);
+
+        for(int i = 0 ; i < s1.size() ; i++){
+            s1_count[s1[i] - 'a']++;
+            s2_count[s2[i] - 'a']++;
+        }
+        if (s1_count == s2_count) {
+            return true;
+        }
+        for(int i = s1.size() ; i < s2.size() ; i++){
+            int right = i;
+            int left = i - s1.size();
+
+            s2_count[s2[left] - 'a']--;
+            s2_count[s2[right] - 'a']++;
+            
+
+            if(s1_count == s2_count){
+                return true;
+            }
+        }
+      return false;  
+    }
+};
