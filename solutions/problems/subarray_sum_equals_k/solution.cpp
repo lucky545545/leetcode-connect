54cf053fae73ab885d8f4b05f673abe48b85d313
lucky545545 <143895062+lucky545545@users.noreply.github.com>
2026-07-27 10:04:53 +0000

    Sync LeetCode submission - Subarray Sum Equals K (cpp)

--- solutions/problems/subarray_sum_equals_k/solution.cpp
+++ solutions/problems/subarray_sum_equals_k/solution.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> prefix(n);
-        prefix[0] = nums[0]; 
-        for(int i = 1 ; i < n ; i++){
-            prefix[i] = prefix[i-1] + nums[i];
-        }
-        unordered_map<int , int> mp;
-        int ans = 0;
-        for(int i = 0 ; i < n ; i++){
-            if(prefix[i] == k){
-                ans++;
-            }
-            if(mp.find(prefix[i] - k) != mp.end()){
-                ans = ans + mp[prefix[i] - k];
-            }
-            mp[prefix[i]]++;
-        }
-        return ans;
-
-        
+        unordered_map<int , int> freq;
+        freq[0] = 1;
+        int prefixsum = 0;
+        int count = 0;
+        for(int i = 0 ; i < nums.size() ; i++){
+            prefixsum += nums[i];
+            if(freq.count(prefixsum - k)){
+                count = count + freq[prefixsum - k];
+            } 
+            freq[prefixsum]++;
+        } 
+        return count;
     }
 };
