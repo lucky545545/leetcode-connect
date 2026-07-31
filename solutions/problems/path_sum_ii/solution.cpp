3608a681166aea7b1f0b775d2687c712fb733391
lucky545545 <143895062+lucky545545@users.noreply.github.com>
2026-07-31 18:17:11 +0000

    Sync LeetCode submission - Path Sum II (cpp)

---
+++ solutions/problems/path_sum_ii/solution.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* root , int targetsum , int &sum , vector<vector<int>> &out , vector<int> &currpath){
+        if(root == nullptr){
+            return;
+        }
+        sum += root->val;
+        currpath.push_back(root->val);
+        
+        if(sum == targetsum && root->left == nullptr && root->right == nullptr){
+            out.push_back(currpath);
+        }
+        
+        dfs(root->left , targetsum , sum , out , currpath);
+        dfs(root->right , targetsum , sum , out , currpath);
+
+        
+        currpath.pop_back();
+        sum -= root->val;
+
+        return;
+
+    }
+
+    vector<vector<int>> pathSum(TreeNode* root, int targetsum) {
+        vector<vector<int>> out;
+        vector<int> currpath;
+        int sum = 0; 
+        dfs(root , targetsum , sum , out , currpath);
+        return out;
+        
+    }
+};
