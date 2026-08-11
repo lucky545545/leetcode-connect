6c759d316909a4e604d41429c2529c7dc93a0362
lucky545545 <143895062+lucky545545@users.noreply.github.com>
2026-08-11 11:03:30 +0000

    Sync LeetCode submission - Insert into a Binary Search Tree (cpp)

---
+++ solutions/problems/insert_into_a_binary_search_tree/solution.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root == nullptr){
+            return new TreeNode(val);
+        }
+        if(root->val == val){
+            return nullptr;
+        }
+        if(val > root->val){
+            if(root->right == nullptr){
+                root->right = new TreeNode(val);
+            }
+            insertIntoBST(root->right , val);
+        }
+        else{
+            if(root->left == nullptr){
+                root->left = new TreeNode(val);
+            }
+            insertIntoBST(root->left ,val);
+        }
+        return root;
+    }
+};
