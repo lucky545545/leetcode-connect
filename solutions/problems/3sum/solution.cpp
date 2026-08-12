ea0428c6e5c2019ce15f7c092dd87daacd01348f
lucky545545 <143895062+lucky545545@users.noreply.github.com>
2026-08-12 19:37:03 +0000

    Sync LeetCode submission - 3Sum (cpp)

--- solutions/problems/3sum/solution.cpp
+++ solutions/problems/3sum/solution.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> out;
         sort(nums.begin() , nums.end());
-        for(int i = 0 ; i < nums.size() ; i++){
+        vector<vector<int>> out;
+        for(int i = 0 ; i < nums.size()-1 ; i++){
             if(i > 0 && nums[i] == nums[i-1]){
                 continue;
             }
             int j = i+1;
             int k = nums.size()-1;
-            while(j<k){
-                int um = nums[i] + nums[j] + nums[k];
-                if(um > 0){
+            
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum > 0){
                     k--;
                 }
-                else if(um < 0){
+                else if(sum < 0){
                     j++;
                 }
                 else{
-                    out.push_back( {nums[i] , nums[j] , nums[k]});
+                    out.push_back({nums[i] , nums[j] , nums[k]});
                     j++;
                     k--;
-                
-                while(nums[j] == nums[j-1] && j<k)j++;
-                while(nums[k] == nums[k+1] && j<k)k--;
+                    while(j < k && nums[j] == nums[j-1]){
+                        j++;
+                    }
+                    while(j < k && nums[k] == nums[k+1]){
+                        k--; 
+                    }
                 }
             }
-
         }
-        return out; 
-        
+        return out;
     }
 };
