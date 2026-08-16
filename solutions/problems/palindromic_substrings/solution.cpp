c2693738b61fb960be2b2ea0e3d975dadfeefa9a
lucky545545 <143895062+lucky545545@users.noreply.github.com>
2026-08-16 10:18:11 +0000

    Sync LeetCode submission - Palindromic Substrings (cpp)

--- solutions/problems/palindromic_substrings/solution.cpp
+++ solutions/problems/palindromic_substrings/solution.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int count = 0;
-    void expand(string s ,int left ,int right){
-        while(left>=0 && right< s.size() && s[left] == s[right]){
+    void expand(string s , int left , int right){
+        while(left >= 0 && right <= s.size() && s[left] == s[right]){
             count++;
             left--;
             right++;
@@ -10,9 +10,10 @@ public:
     }
     int countSubstrings(string s) {
         for(int i = 0 ; i < s.size() ; i++){
-            expand(s, i , i);
-            expand(s, i , i+1);
+            expand(s , i , i);
+            expand(s , i , i+1);
         }
         return count;
+
     }
 };
