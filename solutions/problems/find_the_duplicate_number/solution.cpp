bce96946ce9d567f5779b5721e80e498a227d95b
lucky545545 <143895062+lucky545545@users.noreply.github.com>
2026-08-16 10:49:12 +0000

    Sync LeetCode submission - Find the Duplicate Number (cpp)

--- solutions/problems/find_the_duplicate_number/solution.cpp
+++ solutions/problems/find_the_duplicate_number/solution.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int , int> mp;
-        for(int i = 0 ; i < nums.size() ; i++){
-            mp[nums[i]]++;
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];  //[1,3,4,2,2]
+        }while(slow != fast);
+        int slow2 = nums[0];
+
+        while(slow2 != slow){
+            slow = nums[slow];
+            slow2 = nums[slow2];
         }
-        int ans = -1;
-        for(auto x : mp){
-            if(x.second > 1){
-                ans = x.first;
-            }
-        }
-        return ans;
+        
+        return slow;
     }
 };
