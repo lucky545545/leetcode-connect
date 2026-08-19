8949d2d89bc669a02a6c9877e171e91a95d77a43
lucky545545 <143895062+lucky545545@users.noreply.github.com>
2026-08-19 13:36:26 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

--- solutions/problems/fibonacci_number/solution.cpp
+++ solutions/problems/fibonacci_number/solution.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
+    int memo(int n , vector<int>& dp){
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        if(n <= 1){
+            return n;
+        }
+        return dp[n] = memo(n-1 , dp) + memo(n-2 , dp);
+    }
     int fib(int n) {
-        if(n == 0){
-            return 0;
-        }
-        if(n == 1){
-            return 1;
-        }
-        return fib(n-1) + fib(n-2);
+        vector<int> dp(n+1,-1);
+        return memo(n , dp);
     }
 };
