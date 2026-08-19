7227ab1c849a09d9c08e843d70489cce18e89b15
lucky545545 <143895062+lucky545545@users.noreply.github.com>
2026-08-19 13:52:45 +0000

    Sync LeetCode submission - Climbing Stairs (cpp)

--- solutions/problems/climbing_stairs/solution.cpp
+++ solutions/problems/climbing_stairs/solution.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int memo(int n , vector<int>& dp){
-        if(n == 2){
-            return 2;
-        }
-        if(n == 1){
-            return 1;
+        if(n <= 2){
+            return n;
         }
         if(dp[n] != -1){
             return dp[n];
         }
         return dp[n] = memo(n-1 , dp) + memo(n-2 , dp);
     }
-
     int climbStairs(int n) {
         vector<int> dp(n+1 , -1);
-        return memo(n , dp);
+        return memo(n,dp);
     }
 };
